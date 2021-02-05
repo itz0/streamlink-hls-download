3bdc500c9c2fbaf5ad6bec1c5d9dc33f6f727fc2
itz0 <75608529+itz0@users.noreply.github.com>
2021-02-05 10:41:01 +0000

    Update Streamlink.bat

--- Streamlink.bat
+++ Streamlink.bat
@@ -1,4 +1,5 @@
 @echo off
+rem del video.ts
 set VLC="C:\Program Files\VideoLAN\VLC\vlc.exe"
 set MPC="C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe"
 set /p yturl=Please insert HLS m3u8 playlist full URL link (right-click): 
