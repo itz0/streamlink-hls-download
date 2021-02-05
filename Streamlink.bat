ab33d1a9cf0d1c0e5f907714a0e35037c87c7ac1
itz0 <75608529+itz0@users.noreply.github.com>
2021-02-05 10:40:42 +0000

    Update Streamlink.bat

--- Streamlink.bat
+++ Streamlink.bat
@@ -3,6 +3,11 @@ set VLC="C:\Program Files\VideoLAN\VLC\vlc.exe"
 set MPC="C:\Program Files (x86)\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe"
 set /p yturl=Please insert HLS m3u8 playlist full URL link (right-click): 
 streamlink -o video.ts --hls-segment-threads 10 hls://%yturl:&=^&% best
-rem %VLC% --meta-title=streamlink kino.ts
+cls
+echo Downloaded file video.ts, press any key and check dir
+rem %VLC% --meta-title=streamlink video.ts
 rem %MPC% video.ts
 rem del video.ts
+pause
+cd /d %~dp0
+start .
